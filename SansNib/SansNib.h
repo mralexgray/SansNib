942e1ab1160da23e563304796da4c1bae3c10f5e
Alex Gray <alex@mrgray.com>
2014-01-20 13:17:40 -0500

    major structural revamp of code, workspace, build -> dynamic lib, etc.

---
+++ SansNib/SansNib.h
@@ -0,0 +1,13 @@
+//
+//  SansNib.h
+//  SansNib
+//
+//  Created by Alex Gray on 10/28/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SansNib : NSObject
+
+@end
---
+++ SansNib/SansNib.m
@@ -0,0 +1,13 @@
+//
+//  SansNib.m
+//  SansNib
+//
+//  Created by Alex Gray on 10/28/13.
+//
+//
+
+#import "SansNib.h"
+
+@implementation SansNib
+
+@end
